df4a7e9c6a0018c1d7558126a563d7c5966cab8e
Jagjot Saini <jsaini24@my.whitworth.edu>
2024-04-09 10:27:28 -0700

    fam

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/test2.gif';
+import fp from '../../assets/familypromise1.png'
+//import bg from '../../assets/test2.gif';
 import {Link} from 'react-scroll';
 import Hirelogo from '../../assets/Hireme.png';
 
@@ -8,13 +9,13 @@ const intro = () => {
   return (
      <section id="intro">
         <div className='introContent'>
-            <span className="hello">Hello,</span>
-            <span className="introText">I'm <span className="introName">Jagjot Singh</span>
-            <br/>Full-Stack Developer</span>
+            <span className="introName">Zapper,</span>
+            <span className="hello">A fantastic tool to help predict homelessness<span className="introName"></span>
+            <br/></span>
             <p className="introPara"> A software development engineer with a strong user focus, can-do attitude, and desire to learn new things.<br/> Experienced in creating websites,full-stack projects, business strategies, requirements specifications,<br/> and user manuals as well as mobile and web applications, and experience in several VHDL projects</p>
             <Link><button className="btn"><img src={Hirelogo} alt="Hire" className='Hirelogo'/>Hire Me</button></Link>
         </div>
-        <img src={bg} alt="" className="bg"/>
+        <img src={fp} alt="" className="fp"/>
      </section>
   )
 }
