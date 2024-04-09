027b7c28656a6d50690087ad9444878cbfc3c7ef
Jagjot Saini <jsaini24@my.whitworth.edu>
2024-04-09 09:43:11 -0700

    try2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
-import Contact from "./components/Contact/contact";
+//import Contact from "./components/Contact/contact";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
       <Intro/>
       <Skills/>
-      <Contact/>
+   
     </div>
     
   );
