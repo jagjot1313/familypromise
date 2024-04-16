53931d9ddf8c9b102d7aee4189f5842d276252a2
Jagjot Saini <jsaini24@my.whitworth.edu>
2024-04-15 20:07:21 -0700

    clearing_errors

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,42 +2,6 @@ import React from "react";
 import "./intro.css";
 import fp from "../../assets/familypromise1.png";
 //import bg from '../../assets/test2.gif';
-<<<<<<< HEAD
-import { Link } from "react-scroll";
-import Hirelogo from "../../assets/Hireme.png";
-
-const intro = () => {
-  return (
-    <section id="intro">
-      <div className="introContent">
-        <span className="introName">Zapper,</span>
-        <span className="hello">
-          A fantastic tool to help predict homelessness
-          <span className="introName"></span>
-          <br />
-        </span>
-        <p className="introPara">
-          {" "}
-          A software development engineer with a strong user focus, can-do
-          attitude, and desire to learn new things.
-          <br /> Experienced in creating websites,full-stack projects, business
-          strategies, requirements specifications,
-          <br /> and user manuals as well as mobile and web applications, and
-          experience in several VHDL projects
-        </p>
-        <Link>
-          <button className="btn">
-            <img src={Hirelogo} alt="Hire" className="Hirelogo" />
-            Hire Me
-          </button>
-        </Link>
-      </div>
-      <img src={"../../assets/familypromise1.png"} className="familypromise"/>
-      <img src={fp} alt="" className="fp" />
-    </section>
-  );
-};
-=======
 import {Link} from 'react-scroll';
 import contactus from '../../assets/contactus.png';
 
@@ -55,6 +19,6 @@ const intro = () => {
      </section>
   )
 }
->>>>>>> b3a729fa9126c62dfca7b8d4f3b49e8cad76b877
+
 
 export default intro;
